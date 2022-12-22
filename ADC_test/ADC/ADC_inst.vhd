c42f638991d9c997534a4a74ff22788daed035ff
Hugo J <aom921@hotmail.com>
2022-12-22 14:29:48 -0600

    Modifiqué el componenete del convertidor A/D, de manera que ahora tiene señales de depuracion que permiten ver dentro de la System Console la señal de audio recibida. Además de cambios minimos para que el codigo se comporte de la misma manera.

--- ADC_test/ADC/ADC_inst.vhd
+++ ADC_test/ADC/ADC_inst.vhd
@@ -1,33 +1,25 @@
 	component ADC is
 		port (
-			clk_adc_clk                : in  std_logic                     := 'X';             -- clk
-			reset_reset_n              : in  std_logic                     := 'X';             -- reset_n
-			adc_command_valid          : in  std_logic                     := 'X';             -- valid
-			adc_command_channel        : in  std_logic_vector(4 downto 0)  := (others => 'X'); -- channel
-			adc_command_startofpacket  : in  std_logic                     := 'X';             -- startofpacket
-			adc_command_endofpacket    : in  std_logic                     := 'X';             -- endofpacket
-			adc_command_ready          : out std_logic;                                        -- ready
-			adc_response_valid         : out std_logic;                                        -- valid
-			adc_response_channel       : out std_logic_vector(4 downto 0);                     -- channel
-			adc_response_data          : out std_logic_vector(11 downto 0);                    -- data
-			adc_response_startofpacket : out std_logic;                                        -- startofpacket
-			adc_response_endofpacket   : out std_logic                                         -- endofpacket
+			adc_response_valid         : out std_logic;                            -- valid
+			adc_response_startofpacket : out std_logic;                            -- startofpacket
+			adc_response_endofpacket   : out std_logic;                            -- endofpacket
+			adc_response_empty         : out std_logic_vector(0 downto 0);         -- empty
+			adc_response_channel       : out std_logic_vector(4 downto 0);         -- channel
+			adc_response_data          : out std_logic_vector(11 downto 0);        -- data
+			clk_adc_clk                : in  std_logic                     := 'X'; -- clk
+			reset_reset_n              : in  std_logic                     := 'X'  -- reset_n
 		);
 	end component ADC;
 
 	u0 : component ADC
 		port map (
-			clk_adc_clk                => CONNECTED_TO_clk_adc_clk,                --      clk_adc.clk
-			reset_reset_n              => CONNECTED_TO_reset_reset_n,              --        reset.reset_n
-			adc_command_valid          => CONNECTED_TO_adc_command_valid,          --  adc_command.valid
-			adc_command_channel        => CONNECTED_TO_adc_command_channel,        --             .channel
-			adc_command_startofpacket  => CONNECTED_TO_adc_command_startofpacket,  --             .startofpacket
-			adc_command_endofpacket    => CONNECTED_TO_adc_command_endofpacket,    --             .endofpacket
-			adc_command_ready          => CONNECTED_TO_adc_command_ready,          --             .ready
 			adc_response_valid         => CONNECTED_TO_adc_response_valid,         -- adc_response.valid
+			adc_response_startofpacket => CONNECTED_TO_adc_response_startofpacket, --             .startofpacket
+			adc_response_endofpacket   => CONNECTED_TO_adc_response_endofpacket,   --             .endofpacket
+			adc_response_empty         => CONNECTED_TO_adc_response_empty,         --             .empty
 			adc_response_channel       => CONNECTED_TO_adc_response_channel,       --             .channel
 			adc_response_data          => CONNECTED_TO_adc_response_data,          --             .data
-			adc_response_startofpacket => CONNECTED_TO_adc_response_startofpacket, --             .startofpacket
-			adc_response_endofpacket   => CONNECTED_TO_adc_response_endofpacket    --             .endofpacket
+			clk_adc_clk                => CONNECTED_TO_clk_adc_clk,                --      clk_adc.clk
+			reset_reset_n              => CONNECTED_TO_reset_reset_n               --        reset.reset_n
 		);
 
--- ADC_test/ADCtst.vhd
+++ ADC_test/ADCtst.vhd
@@ -27,6 +27,7 @@ signal adc_rdata : std_logic_vector(11 downto 0);
 
 signal adc_cvalid : std_logic;
 signal adc_cchannel : std_logic_vector(4 downto 0);
+signal adc_rempty : std_logic_vector(0 downto 0);
 
 --	 display 
 signal sal_disp0, sal_disp1, sal_disp2 : std_logic_vector(3 downto 0) := (others => '0');
@@ -34,18 +35,14 @@ signal sal_disp0, sal_disp1, sal_disp2 : std_logic_vector(3 downto 0) := (others
 
     component ADC is
         port (
-            clk_adc_clk                : in  std_logic                     := 'X';             -- clk
-            reset_reset_n              : in  std_logic                     := 'X';             -- reset_n
-            adc_command_valid          : in  std_logic                     := 'X';             -- valid
-            adc_command_channel        : in  std_logic_vector(4 downto 0)  := (others => 'X'); -- channel
-            adc_command_startofpacket  : in  std_logic                     := 'X';             -- startofpacket
-            adc_command_endofpacket    : in  std_logic                     := 'X';             -- endofpacket
-            adc_command_ready          : out std_logic;                                        -- ready
-            adc_response_valid         : out std_logic;                                        -- valid
-            adc_response_channel       : out std_logic_vector(4 downto 0);                     -- channel
-            adc_response_data          : out std_logic_vector(11 downto 0);                    -- data
-            adc_response_startofpacket : out std_logic;                                        -- startofpacket
-            adc_response_endofpacket   : out std_logic                                         -- endofpacket
+           	adc_response_valid         : out std_logic;                            -- valid
+           	adc_response_startofpacket : out std_logic;                            -- startofpacket
+           	adc_response_endofpacket   : out std_logic;                            -- endofpacket
+           	adc_response_empty         : out std_logic_vector(0 downto 0);         -- empty
+           	adc_response_channel       : out std_logic_vector(4 downto 0);         -- channel
+           	adc_response_data          : out std_logic_vector(11 downto 0);        -- data
+            clk_adc_clk                : in  std_logic                     := 'X'; -- clk
+            reset_reset_n              : in  std_logic                     := 'X'  -- reset_n
         );
     end component ADC;	
 
@@ -53,25 +50,21 @@ begin
 
 	u0 : component ADC
         port map (
-            ADCLK                ,    --clk_adc.clk
-            RESET              ,      --reset.reset_n
-            adc_cvalid          ,     -- adc_command.valid
-            adc_cchannel        ,     --.channel
-            adc_csop  ,  --             .startofpacket
-            adc_ceop    ,    --         .endofpacket
-            adc_ready         ,       --.ready
-            adc_valid      ,         -- adc_response.valid
-            adc_rchannel     ,       --.channel
-            adc_rdata        ,       --.data
-            adc_sop,  --             	.startofpacket
-            adc_eop     --             .endofpacket
+            adc_valid,
+			adc_sop,
+			adc_eop,
+			adc_rempty,
+			adc_rchannel,
+			adc_rdata,
+			ADCLK,
+			RESET
         );
 
 		  
-			adc_cvalid <= '1';
-			adc_cchannel <= "00001"; -- ??? ESto esta raro, no se cual sea la dedicated analog input pin, usando el canal "1" corresponde al pin 0 de la fpga
-			adc_csop <= '1';
-			adc_ceop <= '1';
+			--adc_cvalid <= '1';
+			--adc_cchannel <= "00001"; -- ??? ESto esta raro, no se cual sea la dedicated analog input pin, usando el canal "1" corresponde al pin 0 de la fpga
+			--adc_csop <= '1';
+			--adc_ceop <= '1';
 
 
 
