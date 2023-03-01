1c7466c236f9a0b41a96ed9c0ca79f4d5d05ceee
Aom92 <aom921@hotmail.com>
2023-02-28 21:14:45 -0600

    Agregamos bloque para realizar convolucion al diagrama

--- Effects_Pedal/conv.vhd
+++ Effects_Pedal/conv.vhd
@@ -5,22 +5,46 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
-
 entity conv is
   port (
-    SIGNAL_A : IN std_logic_vector;
-    IR_SIGNAL : IN std_logic_vector;
-    ENABLE_L  : IN std_logic;
-
-    IR_SIGNAL_ADDRESS : out std_logic_vector;
-    CONV : OUT std_logic_vector
+    clk : in std_logic;
+    reset : in std_logic;
+    x : in std_logic_vector(15 downto 0);
+    h : in std_logic_vector(15 downto 0);
+    y : out std_logic_vector(31 downto 0)
   );
-end entity;
+end entity conv;
 
 architecture rtl of conv is
-  
+  signal x_reg : std_logic_vector(15 downto 0);
+  signal h_reg : std_logic_vector(15 downto 0);
+  signal y_reg : unsigned(31 downto 0);
+
+  signal address : unsigned (15 downto 0);
 begin
-end architecture;
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if reset = '1' then
+        x_reg <= (others => '0');
+        h_reg <= (others => '0');
+        y_reg <= (others => '0');
+      else
+        x_reg <= x;
+        h_reg <= h;
+        y_reg <=  unsigned(x_reg) * unsigned(h_reg);
+      end if;
+    end if;
+  end process;
+
+  process(clk)
+  begin
+    if rising_edge(clk) then
+
+    end if;
+
+
+  end process;
+  
+  y <= std_logic_vector(y_reg);
+end architecture rtl;
