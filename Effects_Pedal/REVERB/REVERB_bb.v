56c3d68b477c278c1339f728b9ef7996e59038bf
Aom92 <aom921@hotmail.com>
2023-03-09 19:42:26 -0600

    FIR II IP

---
+++ Effects_Pedal/REVERB/REVERB_bb.v
@@ -0,0 +1,20 @@
+
+module REVERB (
+	ast_sink_data,
+	ast_sink_valid,
+	ast_sink_error,
+	ast_source_data,
+	ast_source_valid,
+	ast_source_error,
+	clk,
+	reset_n);	
+
+	input	[11:0]	ast_sink_data;
+	input		ast_sink_valid;
+	input	[1:0]	ast_sink_error;
+	output	[50:0]	ast_source_data;
+	output		ast_source_valid;
+	output	[1:0]	ast_source_error;
+	input		clk;
+	input		reset_n;
+endmodule
